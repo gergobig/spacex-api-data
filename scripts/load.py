8bc19353698dff881a31e32f70e92fa24fe7206f
gergo.nagy <gergo.nagy0216@gmail.com>
2023-05-13 14:06:26 +0200

    finalize table loading

--- scripts/load.py
+++ scripts/load.py
@@ -1,22 +1,46 @@
 import logging
-import json
 
 import pandas as pd
+from sqlalchemy.types import JSON, TEXT, DOUBLE_PRECISION, BOOLEAN, ARRAY, BIGINT
 
 from scripts.utils import PostgresConnector
 
+SCHEMA = {
+    'fairings': JSON,
+    'links': JSON,
+    'static_fire_date_utc': TEXT,
+    'static_fire_date_unix': DOUBLE_PRECISION,
+    'net': BOOLEAN,
+    'window': DOUBLE_PRECISION,
+    'rocket': TEXT,
+    'success': DOUBLE_PRECISION,
+    'failures': ARRAY(JSON),
+    'details': TEXT,
+    'crew': ARRAY(JSON),
+    'ships': ARRAY(TEXT),
+    'capsules': ARRAY(TEXT),
+    'payloads': ARRAY(TEXT),
+    'launchpad': TEXT,
+    'flight_number': BIGINT,
+    'name': TEXT,
+    'date_utc': TEXT,
+    'date_unix': BIGINT,
+    'date_local': TEXT,
+    'date_precision': TEXT,
+    'upcoming': BOOLEAN,
+    'cores': ARRAY(JSON),
+    'auto_update': BOOLEAN,
+    'tbd': BOOLEAN,
+    'launch_library_id': TEXT,
+    'id': TEXT,
+}
+
+
 def main():
+    data = pd.read_json('spacex.json')
     with PostgresConnector() as pg:
-        data = pd.read_json('spacex.json')
-        pg.load_data(data, 'api')
-
-
-
-
-
-
-
-
+        pg.load_data(data, table_name='api', schema=SCHEMA)
+    
 
 
 if __name__ == '__main__':
--- scripts/utils/__init__.py
+++ scripts/utils/__init__.py
@@ -1,6 +1,9 @@
+from typing import Dict
+
 import psycopg2
 import pandas as pd
 from sqlalchemy import create_engine
+
 
 class PostgresConnector:
     def __init__(self, host='localhost', database='spacex', user='gbig', password='gbig'):
@@ -12,10 +15,7 @@ class PostgresConnector:
 
     def __enter__(self):
         self.conn = psycopg2.connect(
-            host=self.host,
-            database=self.database,
-            user=self.user,
-            password=self.password
+            host=self.host, database=self.database, user=self.user, password=self.password
         )
         return self
 
@@ -35,7 +35,14 @@ class PostgresConnector:
     def delete(self, query):
         return self.update(query)
 
-    def load_data(self, df: pd.DataFrame, table_name: str):
-        engine = create_engine(f'postgresql+psycopg2://{self.user}:{self.password}@{self.host}:5432/{self.database}')
-        df.to_sql(table_name, engine, if_exists='replace', index=False)
-
+    def load_data(self, df: pd.DataFrame, table_name: str, schema: Dict[str, str]):
+        engine = create_engine(
+            f'postgresql+psycopg2://{self.user}:{self.password}@{self.host}:5432/{self.database}'
+        )
+        df.to_sql(
+            table_name,
+            engine,
+            if_exists='replace',
+            index=False,
+            dtype=schema,
+        )
